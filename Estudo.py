d136cc04061f80963fcf42a2651f1840b4668881
Iurexz <131163902+Iurexz@users.noreply.github.com>
2023-08-29 08:37:24 -0300

    Add files via upload

---
+++ Estudo.py
@@ -0,0 +1,35 @@
+"""""
+print("Escolha uma opção:")
+print("1 - Opção 1")
+print("2 - Opção 2")
+print("3 - Opção 3")
+opcao = int(input("Opção"))
+
+if opcao == 1:
+    print("ggg") #código para opção 1
+elif opcao == 2:
+    print("jjjj") #código para opção 2
+elif opcao == 3:
+    print("sair") #código para opção 3
+else:
+    print ("Opção inválida.")
+"""
+
+""""""
+opcao=int
+while opcao !=3:
+      print("Escolha uma opção: ")
+      print("1 - Adicionar um novo item")
+      print("2 - Ver todos os itens")
+      print("3 - Sair")  
+      opcao = int(input("Opção escolhida: "))
+
+if opcao == 1:
+      print("ffff")
+elif opcao == 2:
+        print("jaj")
+elif opcao == 3:
+        print("Encerrando o aplicativo...")
+else:
+        print("Opção inválida.")
+""""""
